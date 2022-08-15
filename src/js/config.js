84e25d2bec8d3c4c9937af361ecde1c89005fa9c
dima240-hub <emi_emilian_24@yahoo.com>
2022-08-15 06:58:41 -0700

    Inital commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const PAGE = 1;
+export const KEY = `441150b3-efd5-4520-9ce1-86b34697acc6`;
+export const MODAL_CLOSE_SEC = 2.5;
